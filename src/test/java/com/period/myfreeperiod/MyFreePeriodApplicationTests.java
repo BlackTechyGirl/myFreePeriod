089f64953367ccc596eab7dad6f24a98885a120f
martha danladi <marthadanladi>
2023-06-06 22:32:23 +0100

    commit

--- src/test/java/com/period/myfreeperiod/MyFreePeriodApplicationTests.java
+++ src/test/java/com/period/myfreeperiod/MyFreePeriodApplicationTests.java
@@ -12,10 +12,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 class MyFreePeriodApplicationTests {
 
+
+    @Test
+    void contextLoads() {
+    }
+
     @Test
     void testDatabaseConnection(){
         DriverManagerDataSource dataSource =
-                new DriverManagerDataSource("jdbc:mysql://127.0.0.1:3306/uberdeluxe");
+                new DriverManagerDataSource("jdbc:mysql://localhost:3306/my_free_period_db");
         try {
             Connection connection = dataSource.getConnection("Martha", "DrDanladi@@2020");
             System.out.println(connection);
